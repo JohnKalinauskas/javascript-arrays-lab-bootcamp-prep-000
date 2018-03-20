f976d3af0c19cfb510095e4878fdb83b9a708100
John Kalinauskas <jkalinauskas@live.com>
2018-03-20 16:50:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const app = "I don't do much."
 kittens =["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  return kittens.
+  return kittens.push("Ralph")
   
   
   
 }
+
