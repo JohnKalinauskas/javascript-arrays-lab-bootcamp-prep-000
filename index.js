c3fb4e3603fb177f07d2d8b00c2d3026bac7773d
John Kalinauskas <jkalinauskas@live.com>
2018-03-20 17:33:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function prependKitten(name) {
   return ["Arnold", ...kittens]
 }
 function removeLastKitten(name) {
-  return kittens.slice (2, kittens.length - 1)
+  return slice (2, kittens.length - 1)
 }
 
 
